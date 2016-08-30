0e612b7c4a2ebb277d759b455474a24977e5f3ea
paulunits <obsolete.units@gmail.com>
2016-08-29 17:47:22 -0700

    Finished toggle views for artists, albums, and genres

--- public/js/application.js
+++ public/js/application.js
@@ -26,37 +26,54 @@ $(document).ready(function() {
   } 
 });
  $('#al').on("click", function(event) {
-	event.preventDefault();
-	$('#stooge2').hide();
-  // $( this ).off( event );
+  if( $('#stooge2').is(':visible') ) {
+     event.preventDefault();
+     $('#stooge2').hide();
+      // $( this ).off( event );
 
-	var ajaxRequest = $.ajax ({
+     var ajaxRequest = $.ajax ({
       method: 'GET',
       url: 'albums',
       data: $(this).serialize()
-    })
+     })
 
-    console.log($(this).serialize());
 
-    ajaxRequest.done(function(data) {
-      $('#aldis').html(data);
-    })
+      ajaxRequest.done(function(data) {
+        $('#aldis').html(data);
+      })
+  }
+  else
+  {
+    event.preventDefault();
+    $('#stooge2').show();
+    $('#aldis').empty();
+
+  } 
 });
-  $('#gen').on("click", function(event) {
-	event.preventDefault();
-	$('#stooge3').hide();
-  // $( this ).off( event );
 
-	var ajaxRequest = $.ajax ({
+  $('#gen').on("click", function(event) {
+    if( $('#stooge3').is(':visible') ) {
+     event.preventDefault();
+     $('#stooge3').hide();
+      // $( this ).off( event );
+
+     var ajaxRequest = $.ajax ({
       method: 'GET',
       url: 'genres',
       data: $(this).serialize()
-    })
+     })
 
-    console.log($(this).serialize());
 
-    ajaxRequest.done(function(data) {
-      $('#gendis').html(data);
-    })
-});
+      ajaxRequest.done(function(data) {
+        $('#gendis').html(data);
+      })
+  }
+  else
+  {
+    event.preventDefault();
+    $('#stooge3').show();
+    $('#gendis').empty();
+
+  } 
+ }); 
 });
